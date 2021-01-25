4c4112889f1bd51cfa5d8e89dff7d0d99253524a
mukul-prasad-au9 <mukulp0783@gmail.com>
2021-01-25 22:05:28 +0530

    home and navbar added

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,18 +1,23 @@
+import image from "./IMG_20200926_093609-removebg-preview (4).png";
+import "./css/image.css";
 const Home=()=>{
     return(
         <>
-            <div className="container d-flex flex-row align-item-center">
-                <div className="d-flex flex-column">
-                    <h3>HELLO!</h3>
-                    <h1>I AM MUKUL PRASAD</h1>
-                    <h2>FULL STACK WEB DEVELOPER</h2>
-                    <p>kjssjdfygsjfbsdbcsbdbcsgfhshbcbncbsbchgshbcnzcbhgsvchgsvhcsh</p>
-                    <div className="d-flex flex-row justify-content-between">
-                        <button className="btn btn-info">contact me</button>
-                        <button className="btn btn-light">view resume</button>
+            <div className="container d-flex flex-row justify-content-between">
+                <div className="d-flex align-items-center">
+                    <div className="flex-column">
+                        <h3>HELLO!</h3>
+                        <h1>I AM MUKUL PRASAD</h1>
+                        <h2>FULL STACK WEB DEVELOPER</h2>
+                        <p>Looking for a position where I can integrate strategies to develop myself</p> 
+                        <p>and the organization and also expand my knowledge and company’s production.</p>
+                        <div className="d-flex flex-row justify-content-around">
+                            <button className="btn btn-info">contact me</button>
+                            <button className="btn btn-dark">view resume</button>
+                        </div>
                     </div>
                 </div>
-                <img src="" alt="profile"/>
+                <img src={image} alt="profile" className="image"/>
             </div>
         </>
     )
--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -2,14 +2,16 @@ import { Link } from "react-router-dom"
 
 const Navbar =()=>{
     return(
-        <div className="d-flex flex-row">
-            <h4 className="col-2">MUKUL</h4>
+        <div className="d-flex flex-row py-2" style={{height:"20%",backgroundColor:"grey"}}>
+            <h4 className="">MUKUL PRASAD</h4>
             <div className="d-flex justify-content-center">
-                <Link to="/">Home</Link>
-                <Link to="/about">About</Link>
-                <Link to="/skills">Skills</Link>
-                <Link to="/works">Works</Link>
-                <Link to="/resume">Resume</Link>
+                <div className="d-flex flex-row justify-content-between">
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/about">About</Link></li>
+                    <li><Link to="/skills">Skills</Link></li>
+                    <li><Link to="/works">Works</Link></li>
+                    <li><Link to="/resume">Resume</Link></li>
+                </div>
             </div>
         </div>
     )
